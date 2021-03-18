d6f2dad539c5e317ce3294970a011b2169ae8f83
harry <harrisongitau3@gmail.com>
2021-03-18 17:58:44 +0300

    Update edit profile redirect

--- app/routes.py
+++ app/routes.py
@@ -119,13 +119,13 @@ def before_request():
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('edit_profile'))
+        return redirect(url_for('user', username=current_user.username))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
