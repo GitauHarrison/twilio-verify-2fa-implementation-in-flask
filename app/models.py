86fd229eab4e340089742e06c23dc56c7c10db09
harry <harrisongitau3@gmail.com>
2021-03-18 18:59:07 +0300

    Add phone field to db

--- app/models.py
+++ app/models.py
@@ -16,6 +16,10 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    verification_phone = db.Column(db.String(16))
+
+    def two_factor_enabled(self):
+        return self.verification_phone is not None
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
